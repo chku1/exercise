f1bc92560b95cf9beee666f2199839c2d7e18767
chku1 <90269423+chku1@users.noreply.github.com>
2021-11-24 21:03:08 +0400

    Add files via upload

--- 3.js
+++ 3.js
@@ -10,8 +10,23 @@ function Triangle(a, b, c) {
   this.c = c;
 }
 
-Triangle.prototype = shape;
+Triangle.prototype = shape; 
+Triangle.prototype.constructor = Triangle; 
+Triangle.prototype.type = 'triangle'; 
+
+
 Triangle.prototype.getPerimeter = function() {
   return this.a + this.b + this.c;
 };
-Triangle.prototype.constructor = Triangle;
+
+for (var i in t) { 
+    if (t.hasOwnProperty(i)) { 
+      console.log(i, '=', t[i]); 
+   } 
+} 
+
+Array.prototype.shuffle = function () { 
+    return this.sort(function () { 
+      return Math.random() - 0.5; 
+    }); 
+};
